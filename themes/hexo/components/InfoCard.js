ad5b5814cfd4536cde25985d68be2299b80c7c65
Nico <90017933+nijun1993@users.noreply.github.com>
2022-09-22 21:19:56 +0800

    Update InfoCard.js

--- themes/hexo/components/InfoCard.js
+++ themes/hexo/components/InfoCard.js
@@ -7,12 +7,7 @@ export function InfoCard (props) {
   const { className, siteInfo } = props
   const router = useRouter()
   return <Card className={className}>
-    <div
-//       className='justify-center items-center flex hover:rotate-45 py-6 hover:scale-105 dark:text-gray-100 font-sans transform duration-200 cursor-pointer'
-//       onClick={() => {
-//         router.push('/')
-//       }}
-    >
+    <div>
     {/* eslint-disable-next-line @next/next/no-img-element */}
     <img src={siteInfo?.icon} className='rounded-full' width={120}/>
     </div>
