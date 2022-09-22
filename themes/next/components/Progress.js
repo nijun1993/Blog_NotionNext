e31a7bb527c351dfe4f6f240d55098c8b0b77376
Nico <90017933+nijun1993@users.noreply.github.com>
2022-09-23 00:34:09 +0800

    Update Progress.js

--- themes/next/components/Progress.js
+++ themes/next/components/Progress.js
@@ -28,15 +28,15 @@ const Progress = ({ targetRef, showPercent = true }) => {
   }, [percent])
 
   return (
-    <div className="h-4 w-full shadow-2xl  font-sans">
-      <div
-        className="h-4  duration-200"
+    <div className="">
+//       <div
+//         className="h-4  duration-200"
 //         style={{ width: `${percent}%` }}
-      >
-        {showPercent && (
-          <div className="text-right text-white text-xs">{percent}%</div>
-        )}
-      </div>
+//       >
+//         {showPercent && (
+//           <div className="text-right text-white text-xs">{percent}%</div>
+//         )}
+//       </div>
     </div>
   )
 }
