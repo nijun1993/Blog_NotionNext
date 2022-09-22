ac3e66bf123321a0a2c9c3c4d3e64cfe3ef75282
Nico <90017933+nijun1993@users.noreply.github.com>
2022-09-23 01:06:46 +0800

    Update BlogPostArchive.js

--- themes/next/components/BlogPostArchive.js
+++ themes/next/components/BlogPostArchive.js
@@ -24,7 +24,7 @@ const BlogPostArchive = ({ posts = [], archiveTitle }) => {
           {posts.map(post => (
             <li
               key={post.id}
-              className="border-l-2 p-1 text-xs md:text-base items-center  hover:scale-x-105 hover:border-gray-500 dark:hover:border-gray-300 dark:border-gray-400 transform duration-500"
+              className="border-l-2 p-1 text-xs md:text-base items-center  hover:scale-x-105 hover:border-gray-500 dark:hover:border-gray-300 dark:border-gray-400 transform duration-200"
             >
               <div id={post?.date?.start_date}>
                 <span className="text-gray-400">{post.date?.start_date}</span>{' '}
