351005b66cb1dffd9a783be93ff559aa9230e767
Nico <90017933+nijun1993@users.noreply.github.com>
2022-09-23 09:57:23 +0800

    Update Logo.js

--- themes/next/components/Logo.js
+++ themes/next/components/Logo.js
@@ -6,7 +6,7 @@ const Logo = props => {
   return <Link href='/' passHref>
     <div className={'flex flex-col justify-center items-center cursor-pointer bg-black space-y-3 font-bold ' + className}>
       <div className='font-serif text-xl text-white'> {siteInfo?.title}</div>
-      <div className='text-sans text-gray-300 font-light'> {siteInfo?.description}</div>
+      <div className='font-sans text-gray-300 font-light'> {siteInfo?.description}</div>
     </div>
   </Link>
 }
